d5c90ba2793276de27fe5b7d12d56fd1447623be
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2015-12-06 11:03:47 -0700

    Bringing over initial experimental code for using Jackcess to read data from a mdb file.  Hooked up the testApp call to call App.main.

--- src/main/java/com/mtnaircomputer/atcsmon_dbtools/App.java
+++ src/main/java/com/mtnaircomputer/atcsmon_dbtools/App.java
@@ -1,5 +1,13 @@
 package com.mtnaircomputer.atcsmon_dbtools;
 
+import java.io.File;
+import java.io.IOException;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Table;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Column;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		System.out.println ("Hello World, packages and such");
+		
+		try {
+			Database db = DatabaseBuilder.open(new File("ATCSdb.mdb"));
+
+			try {
+				for(String tableName : db.getTableNames()) {
+					System.out.println("Table name: " + tableName);
+				}
+				
+				Table table = db.getTable("MCP");
+				for(Row row : table) {
+				    System.out.println("Look ma, a row: " + row);
+					
+				    for(Column column : table.getColumns()) {
+						    String columnName = column.getName();
+						    Object value = row.get(columnName);
+						    System.out.println("Column " + columnName + "(" + column.getType() + "): "
+						    		+ (value != null ? value : "null") + " (" 
+						    		+ (value != null ? value.getClass() : "null") + ")");
+				    }
+				}
+			}
+			finally {
+				db.close();
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
