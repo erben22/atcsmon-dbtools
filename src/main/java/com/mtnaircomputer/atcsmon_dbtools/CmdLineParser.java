e7d3cd94e5b50f13d157c53434809f23a6d68078
R. Cody Erben (lookout) <erben22@mtnaircomputer.net>
2015-12-08 00:30:37 -0700

    Adding a CmdLineParser class.

---
+++ src/main/java/com/mtnaircomputer/atcsmon_dbtools/CmdLineParser.java
@@ -0,0 +1,5 @@
+package com.mtnaircomputer.atcsmon_dbtools;
+
+public class CmdLineParser {
+
+}
---
+++ src/main/java/com/mtnaircomputer/atcsmon_dbtools/CmdLineParserTest.java
@@ -0,0 +1,7 @@
+package com.mtnaircomputer.atcsmon_dbtools;
+
+import junit.framework.TestCase;
+
+public class CmdLineParserTest extends TestCase {
+
+}
